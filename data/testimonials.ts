a9ca6a645c2864ddc4e7c46a4e936233eaf3474f
Myriad-Ibrahima-Diouf <ibrahima.diouf@myriadgroup.com>
2025-02-21 16:09:27 +0000

    deploy website

---
+++ data/testimonials.ts
@@ -0,0 +1,82 @@
+interface Testimonial {
+    name: string
+    role: string
+    content: string
+    rating: number
+    initials: string
+  }
+
+export const testimonials: Testimonial[] = [
+    {
+      name: "Mouhamed",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 4,
+      initials: "M",
+    },
+    {
+      name: "Amina",
+      role: "Cliente",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 4,
+      initials: "MW",
+    },
+    {
+      name: "Fatou D",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 3,
+      initials: "MW",
+    },
+    {
+      name: "Mouhamed",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 3,
+      initials: "M",
+    }, 
+    {
+    name: "Amina",
+      role: "Cliente",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 4,
+      initials: "MW",
+    },
+    {
+      name: "Fatou D",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 2,
+      initials: "MW",
+    },
+    {
+      name: "Mouhamed",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 3,
+      initials: "M",
+    },
+    {
+      name: "Amina",
+      role: "Cliente",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 4,
+      initials: "MW",
+    },
+    {
+      name: "Fatou D",
+      role: "GP France",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua quis nostrud exercitation ullamcoLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do",
+      rating: 4,
+      initials: "MW",
+    },
+  ]
